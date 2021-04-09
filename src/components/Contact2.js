fc8f21bc67d3d7696724ee57b8b27251c6135383
Brian Boisvert <b.boisvert@fvfdkhn7mnhx.myfiosgateway.com>
2021-04-08 21:04:47 -0400

    updated

---
+++ src/components/Contact2.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Contact extends React.Component {
+
+
+  render() {
+    return (
+      <span className='logistics2'>
+        <div>hotels:</div>
+      </span>
+    )
+  }
+}
+
+
+
+export default Contact;
--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Contact from './Contact';
+import Contact2 from './Contact2';
 
 class Invitation extends React.Component {
 
@@ -18,10 +19,17 @@ class Invitation extends React.Component {
           <div>Brian & Sam, Wedding</div><br />
           <div>November Sixth 2021</div><br />
           <div>The Foundry, New York City</div><br />
-          <div>Formal Invitation to Follow</div><br />
-          <div className='arrow'>↓</div>
+          <div className='invite'>Formal Invitation to Follow</div>
+          <span className='logistics'>
+            <span className='hotel'>hotel blocks: </span>
+            <a href='https://www.thewilliamvale.com/'>william vale, </a>
+            <a href='https://thehoxton.com/williamsburg/'>williamsburg, </a>
+            <a href='https://www.ravelhotel.com/'>ravel</a>
+            <span className='separator'> // </span>
+            <span className='label'>contact: </span>
+            <a href='mailto:boisvertlight@gmail.com'>boisvertlight@gmail.com</a>
+          </span>
         </div>
-          <div><Contact /></div>
       </div>
     )
   }
